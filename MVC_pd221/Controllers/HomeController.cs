ab385c366d1fa8ff95e30b7d3b32786fc51143a9
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-01-15 16:12:00 +0200

    Add project files.

---
+++ MVC_pd221/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using MVC_pd221.Models;
+using System.Diagnostics;
+
+namespace MVC_pd221.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contacts()
+        {
+            return View(); // ~/Views/Home/Contacts.cshtml
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_pd221/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_pd221.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
