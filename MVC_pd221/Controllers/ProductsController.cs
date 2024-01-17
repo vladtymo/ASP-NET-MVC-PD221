ab703d39b3a68bb174101c0f8b8be0cd7b8411d3
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-01-17 15:51:21 +0200

    Create database with products. Implement product table page.

---
+++ MVC_pd221/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_pd221.Data;
+
+namespace MVC_pd221.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ShopDbContext context;
+
+        public ProductsController()
+        {
+            this.context = new ShopDbContext();
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+
+            return View(products);
+        }
+    }
+}
