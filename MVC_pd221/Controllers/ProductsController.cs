547a162af150b03451f35711ce3c04bf8361fe79
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2024-01-19 16:20:22 +0200

    Implement create product page.

--- MVC_pd221/Controllers/ProductsController.cs
+++ MVC_pd221/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MVC_pd221.Data;
+using MVC_pd221.Data.Entities;
 
 namespace MVC_pd221.Controllers
 {
@@ -19,9 +20,29 @@ namespace MVC_pd221.Controllers
             return View(products);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product model)
+        {
+            // create product in the db
+            context.Products.Add(model);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
         public IActionResult Delete(int id)
         {
             // delete by id
+            var product = context.Products.Find(id);
+            if (product == null) return NotFound();
+
+            context.Products.Remove(product);
+            context.SaveChanges();
 
             return RedirectToAction("Index");
         }
